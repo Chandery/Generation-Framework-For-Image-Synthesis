fix lightning optim&lr bug which involve conflit with torch; init metrics calculation modules

models/CycleGAN.py
@@ -6,6 +6,7 @@ from . import networks
 import itertools
 from utils.image_pool import ImagePool
 import PIL
+import random
 
 class CycleGAN(L.LightningModule):
     def __init__(self, 
@@ -31,11 +32,19 @@ class CycleGAN(L.LightningModule):
                  beta=0.5,
                  scheduler_policy=None,
                  display_freq=10,
+                 val_batch_num=5,
+                 val_batch_total=None,
                  *args,
                  **kwargs):
         super(CycleGAN, self).__init__()
         self.is_Train = is_Train
         self.automatic_optimization = False
+        
+        if is_Train:
+            self.val_batch_idx = [random.randint(0, val_batch_total-1) for _ in range(val_batch_num)]
+            if 0 not in self.val_batch_idx:
+                self.val_batch_idx[-1]=0
+            print(f"val_batch_idx: {self.val_batch_idx}")
         
         self.loss_names = ['D_A', 'D_B', 'G_A', 'G_B', 'cycle_A', 'cycle_B', 'idt_A', 'idt_B']
         self.model_names = ['G_A', 'G_B', 'D_A', 'D_B'] if is_Train else ['G_A', 'G_B']
@@ -76,19 +85,6 @@ class CycleGAN(L.LightningModule):
             
             self.fake_A_pool = ImagePool(pool_size)
             self.fake_B_pool = ImagePool(pool_size)
-            
-            self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), 
-                                                                self.netG_B.parameters()), 
-                                                lr=self.init_lr, 
-                                                betas=(self.beta, 0.999))
-            self.optimizer_D = torch.optim.Adam(itertools.chain(self.netD_A.parameters(), 
-                                                                self.netD_B.parameters()), 
-                                                lr=self.init_lr, 
-                                                betas=(self.beta, 0.999))
-            self.optimizers = [self.optimizer_G, self.optimizer_D]
-            
-            if self.schedule_policy is not None:
-                self.schedulers = [networks.get_scheduler(optimizer, self.schedule_policy) for optimizer in self.optimizers]
         
     def make_G(self, in_channels, out_channels, features, netG, norm, use_dropout, init_type, init_gain):
         return networks.define_G(in_channels, out_channels, features, netG, norm, use_dropout, init_type, init_gain)
@@ -108,16 +104,16 @@ class CycleGAN(L.LightningModule):
         self.forward()
         
         losses = self.get_current_losses()
+        batch_size = batch['A'].size(0)  # 获取batch size
         for name, loss in losses.items():
-            self.log(f'train/{name}', loss, prog_bar=True, logger=True, on_step=True, on_epoch=True, sync_dist=False)
+            self.log(f'train/{name}', loss, prog_bar=True, logger=True, on_step=True, on_epoch=True, sync_dist=False, batch_size=batch_size)
     
         self.optimize_parameters()
-        
         if batch_idx == 0:
             self.update_learning_rate()
     
     def validation_step(self, batch, batch_idx):
-        if batch_idx == 0:
+        if batch_idx in self.val_batch_idx:
             self.set_input(batch)
             self.forward()
             self.get_loss_G()
@@ -125,8 +121,9 @@ class CycleGAN(L.LightningModule):
             self.get_loss_D_B()
             
             losses = self.get_current_losses()
+            batch_size = batch['A'].size(0)  # 获取batch size
             for name, loss in losses.items():
-                self.log(f'val/{name}', loss, sync_dist=False)
+                self.log(f'val/{name}', loss, sync_dist=False, batch_size=batch_size)
             
             
             images = self.get_current_visuals()
@@ -135,7 +132,7 @@ class CycleGAN(L.LightningModule):
                 img = (img + 1) * 127.5
                 img = img.transpose(1, 2, 0)
                 img = img.astype(np.uint8)
-                self.logger.experiment.add_image(name, img, self.global_step, dataformats='HWC')
+                self.logger.experiment.add_image(f'val_images/{name}', img, batch_idx, dataformats='HWC')
             
     def test_step(self, batch, batch_idx):
         self.set_input(batch)
@@ -147,6 +144,25 @@ class CycleGAN(L.LightningModule):
         images = self.get_current_visuals()
         for name, image in images.items():
             self.save_image(image, f'{dir_name}/{name}.png')
+
+    def init_from_ckpt(self, path, ignore_keys=list(), only_model=False):
+        sd = torch.load(path, map_location="cpu")
+        if "state_dict" in list(sd.keys()):
+            sd = sd["state_dict"]
+        keys = list(sd.keys())
+        for k in keys:
+            for ik in ignore_keys:
+                if k.startswith(ik):
+                    print("Deleting key {} from state_dict.".format(k))
+                    del sd[k]
+        missing, unexpected = (
+            self.load_state_dict(sd, strict=False) if not only_model else self.model.load_state_dict(sd, strict=False)
+        )
+        print(f"Restored from {path} with {len(missing)} missing and {len(unexpected)} unexpected keys")
+        if len(missing) > 0:
+            print(f"Missing Keys: {missing}")
+        if len(unexpected) > 0:
+            print(f"Unexpected Keys: {unexpected}")
     
     def save_image(self, image, path):
         image = image.cpu().numpy().squeeze()
@@ -180,8 +196,8 @@ class CycleGAN(L.LightningModule):
         self.loss_D_B = self.loss_D_basic(self.netD_B, self.real_B, fake_A)
     
     def backward_D(self):
-        self.loss_D_A.backward()
-        self.loss_D_B.backward()
+        self.manual_backward(self.loss_D_A)
+        self.manual_backward(self.loss_D_B)
     
     def get_loss_G(self):
         
@@ -209,7 +225,7 @@ class CycleGAN(L.LightningModule):
         return self.loss_G
     
     def backward_G(self):
-        self.loss_G.backward()
+        self.manual_backward(self.loss_G)
         
     def get_current_losses(self):
         losses = {}
@@ -232,31 +248,59 @@ class CycleGAN(L.LightningModule):
                 param.requires_grad = requires_grad
     
     def optimize_parameters(self):
-        self.forward()
-        self.set_requires_grad([self.netD_A, self.netD_B], False)
-        self.optimizer_G.zero_grad()
-        self.get_loss_G()
-        self.backward_G()
-        self.optimizer_G.step()
+        opt_G, opt_D= self.optimizers() # !!!!!!!!! VeryImportant! if dont use this, global_step wil not be updated
         
+        self.set_requires_grad([self.netD_A, self.netD_B], False)
+        opt_G.zero_grad()
+        self.get_loss_G()
+        self.manual_backward(self.loss_G)
+        opt_G.step()
+
         self.set_requires_grad([self.netD_A, self.netD_B], True)
-        self.optimizer_D.zero_grad()
+        opt_D.zero_grad()
         self.get_loss_D_A()
         self.get_loss_D_B()
-        self.backward_D()
-        self.optimizer_D.step()   
-    
+        self.manual_backward(self.loss_D_A)
+        self.manual_backward(self.loss_D_B)
+        opt_D.step()
+        
     def update_learning_rate(self):
-        old_lr = self.optimizers[0].param_groups[0]['lr']
-        for scheduler in self.schedulers:
+        old_lr = self.optimizers()[0].param_groups[0]['lr']
+        
+        # ** check if optimizer has executed step
+        # ? this is for fixing the bug of lightning
+        for optimizer in self.optimizers():
+            if not hasattr(optimizer, '_step_count') or optimizer._step_count == 0:
+                return  # ?if optimizer has not executed step, return
+        
+        # ? update learning rate only if optimizer has executed step
+        for scheduler in self.lr_schedulers():
             if self.schedule_policy.lr_policy == 'plateau':
                 scheduler.step(self.schedule_policy.metric)
             else:
                 scheduler.step()
 
-        lr = self.optimizers[0].param_groups[0]['lr']
+        lr = self.optimizers()[0].param_groups[0]['lr']
         if old_lr != lr:
             print('change learning rate %.7f -> %.7f' % (old_lr, lr))
         
     def configure_optimizers(self):
-        return self.optimizers
+        self.optimizer_G = torch.optim.Adam(itertools.chain(self.netG_A.parameters(), 
+                                                            self.netG_B.parameters()), 
+                                            lr=self.init_lr, 
+                                            betas=(self.beta, 0.999))
+        self.optimizer_D = torch.optim.Adam(itertools.chain(self.netD_A.parameters(), 
+                                                            self.netD_B.parameters()), 
+                                            lr=self.init_lr, 
+                                            betas=(self.beta, 0.999))
+        self.optimizers_list = [self.optimizer_G, self.optimizer_D]
+        
+        if self.schedule_policy is not None:
+            self.schedulers_list = [networks.get_scheduler(optimizer, self.schedule_policy) for optimizer in self.optimizers_list]
+            schedulers = [{
+                'scheduler': scheduler,
+                'interval': 'epoch',
+                'frequency': 1,
+            } for scheduler in self.schedulers_list]
+            return [self.optimizer_G, self.optimizer_D], schedulers
+        return [self.optimizer_G, self.optimizer_D]

scripts/train_CycleGAN.py
@@ -14,19 +14,17 @@ from torch.utils.data import DataLoader
 def main(config):
     config = config["config"]
     config.trainer.max_epochs = config.train.n_epochs + config.train.n_epoch_decay
-    filename="train_CycleGAN-epoch{epoch:02d}-"+f"{config.monitor}"+"{"+"{config.monitor}:.2f"+"}"
+    filename = f"train_CycleGAN-epoch{{epoch:02d}}-{config.monitor.replace('/', '_')}={{{config.monitor}:.2f}}"
     print("save model to", filename)
+    
     checkpoint_callback = ModelCheckpoint(
         monitor=config.monitor,
+        dirpath=config.hydra_path,
         filename=filename,
         save_top_k=1,
         mode="min",
         auto_insert_metric_name=False,
         save_last=True,
-    )
-    checkpoint_callback_last = ModelCheckpoint(
-        dirpath=config.hydra_path,
-        filename="latest",
     )
     
     train_ds = UnalignedDataset(opt=config.DatasetConfig, split="train")
@@ -40,10 +38,15 @@ def main(config):
                       batch_size=config.DatasetConfig.batch_size, 
                       shuffle=False, 
                       num_workers=config.DatasetConfig.num_workers)
+
+    val_batch_total = len(val_ds) // config.DatasetConfig.batch_size
+    model = CycleGAN(**config["CycleGAN"], 
+                     is_Train=True, 
+                     root_dir=config.hydra_path, 
+                     val_batch_total=val_batch_total
+    )
     
-    model = CycleGAN(**config["CycleGAN"], is_Train=True, root_dir=config.hydra_path, display_freq=config.train.display_freq)
-    
-    trainer = L.Trainer(**config["trainer"], callbacks=[checkpoint_callback, checkpoint_callback_last], default_root_dir=config.hydra_path)
+    trainer = L.Trainer(**config["trainer"], callbacks=[checkpoint_callback], default_root_dir=config.hydra_path)
     
     trainer.fit(model=model, train_dataloaders=train_dl, val_dataloaders=val_dl)
 